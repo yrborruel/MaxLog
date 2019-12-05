3f7196f66be20f0544e704e6a737a25de69f76e9
yrborruel <yrborruel@gmail.com>
2019-12-05 17:32:18 -0300

    5-12-2019

--- src/Inicio.java
+++ src/Inicio.java
@@ -1,9 +1,14 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -38,7 +43,7 @@ public class Inicio {
 			st = BaseDatos.createStatement();
 			ResultSet rs = st.executeQuery("SELECT * FROM public.\"MXErrorSolucion\"");
 
-			ArrayList<Error> errores = prepararTXT("D:\\integcheck.log");
+			ArrayList<Error> errores = prepararTXT("D:\\integcheck.txt");
 			ArrayList<String> consultas = new ArrayList<>();
 
 			Map<String, String> erroresDB = new HashMap<String, String>();
@@ -52,8 +57,8 @@ public class Inicio {
 			for (int i = 0; i < errores.size(); i++) {
 
 				String solucions = "" + erroresDB.get(errores.get(i).getError());
-				String descr = "/* \n" + errores.get(i).getError() + " --- " + errores.get(i).getDescrpcion() + " " +errores.get(i).getObjeto()+ "."+ errores.get(i).getAtributo()
-						+ "\n */";
+				String descr = "/* \n" + errores.get(i).getError() + " --- " + errores.get(i).getDescrpcion() + " "
+						+ errores.get(i).getObjeto() + "." + errores.get(i).getAtributo() + "\n */";
 				String consulta = String.format(solucions, errores.get(i).getObjeto(), errores.get(i).getAtributo());
 				if (!solucions.trim().equals("null")) {
 					consultas.add(descr);
@@ -98,111 +103,100 @@ public class Inicio {
 
 	static ArrayList<Error> prepararTXT(String archivo) throws FileNotFoundException, IOException {
 		String cadena;
-		// Leer Fichero
-		File fichero = new File(archivo);
-		FileReader f = new FileReader(fichero);
-		BufferedReader b = new BufferedReader(f);
 		// Exp Regulares
 		Pattern patternError = Pattern.compile("^BMXAA[0-9]{4}E -- Error - BMXAA[0-9]{4}E*$", Pattern.CASE_INSENSITIVE);
-		Pattern patternErrorP = Pattern.compile("^BMXAA[0-9]{4}E*$", Pattern.CASE_INSENSITIVE);
+		Pattern patternErrorP = Pattern.compile("^BMXAA[0-9]{4}E$", Pattern.CASE_INSENSITIVE);
 		Pattern patternError2 = Pattern.compile("^BMXAA[0-9]{4}E -- Error - configure#NoAutoKeyName*$",
 				Pattern.CASE_INSENSITIVE);
 		Pattern patternError3 = Pattern.compile("^BMXAA[0-9]{4}E -- Error - configure#SeqNameNull*$",
 				Pattern.CASE_INSENSITIVE);
 		Pattern patternWar = Pattern.compile("^BMXAA[0-9]{4}W*$", Pattern.CASE_INSENSITIVE);
 		Pattern patternInfo = Pattern.compile("^BMXAA[0-9]{4}I*$", Pattern.CASE_INSENSITIVE);
-		// Escribir ficheros
-		FileWriter ferrores = new FileWriter("d:/errores.txt");
-		PrintWriter err = new PrintWriter(ferrores);
 
-		FileWriter mimomo = new FileWriter("d:/ficheroLog.txt");
-		PrintWriter mo = new PrintWriter(mimomo);
-		
 		ArrayList<Error> lista = new ArrayList<>();
 
-		int cont = 0;
-		while ((cadena = b.readLine()) != null) {
-			
-			cont++;
-			if (cont == 500) {
-				System.gc();
-				cont = 0;
-			}
-			cadena = cadena.trim();
-			mo.println(cadena);
-			if (esErrorP(cadena, patternErrorP))
-				if (esError(cadena, patternError)) {
-					String errorMain = cadena.substring(0, 32);
-					String Desc = cadena.substring(32, cadena.length());
-					while ((cadena = b.readLine()) != null) {
-						if (esErrorP(cadena, patternErrorP) || esError(cadena, patternError)
-								|| esInfo(cadena, patternInfo) || esWar(cadena, patternWar)
-								|| esError2(cadena, patternError2) || esError3(cadena, patternError3))
-							break;
-						else {
-							String atributos = atributo(cadena);
-							if (atributos != null) {
-								err.println(errorMain + "---" + atributos);
-								Error obj = crearObj(errorMain, Desc, atributos);
-								lista.add(obj);
-								System.out.println(errorMain);
+		FileInputStream fis = new FileInputStream(archivo);
+		InputStreamReader isr = new InputStreamReader(fis, "utf8");
+		BufferedReader br = new BufferedReader(isr);
 
-							}
-						}
-					}
+		FileOutputStream fos = new FileOutputStream("d:/errores.txt");
+		OutputStreamWriter isw = new OutputStreamWriter(fos, "utf8");
+		BufferedWriter bw = new BufferedWriter(isw);
+		
+		FileOutputStream fos2 = new FileOutputStream("d:/total.txt");
+		OutputStreamWriter isw2 = new OutputStreamWriter(fos2, "utf8");
+		BufferedWriter bw2 = new BufferedWriter(isw2);
+
+		while ((cadena = br.readLine()) != null) {
+			cadena = cadena.trim();
+			bw2.write(cadena + "\n");
+		}
+		
+		
+		
+		while ((cadena = br.readLine()) != null) {
+			cadena = cadena.trim();
+			bw2.write(cadena + "\n");
+			
+			
+			
+			if (esErrorP(cadena, patternErrorP)) {
+				String errorMain = "";
+				String Desc = "";
+				if (esError(cadena, patternError)) {
+					errorMain = cadena.substring(0, 32);
+					Desc = cadena.substring(32, cadena.length());
 				} else if (esError2(cadena, patternError2)) {
-					String errorMain = cadena.substring(0, 45);
-					String Desc = cadena.substring(45, cadena.length());
-					while ((cadena = b.readLine()) != null) {
-						if (esErrorP(cadena, patternErrorP) || esError(cadena, patternError)
-								|| esInfo(cadena, patternInfo) || esWar(cadena, patternWar)
-								|| esError2(cadena, patternError2) || esError3(cadena, patternError3))
-							break;
-						else {
-							String atributos = atributo(cadena);
-							if (atributos != null) {
-								err.println(errorMain + "---" + atributos);
-								Error obj = crearObj(errorMain, Desc, atributos);
-								lista.add(obj);
-								System.out.println(errorMain);
-							}
-						}
-					}
-				}else if (esError3(cadena, patternError3)) {
-					String errorMain = cadena.substring(0, 43);
-					String Desc = cadena.substring(43, cadena.length());
-					while ((cadena = b.readLine()) != null) {
-						if (esErrorP(cadena, patternErrorP) || esError(cadena, patternError)
-								|| esInfo(cadena, patternInfo) || esWar(cadena, patternWar)
-								|| esError2(cadena, patternError2) || esError3(cadena, patternError3))
-							break;
-						else {
-							String atributos = atributo(cadena);
-							if (atributos != null) {
-								err.println(errorMain + "---" + atributos);
-								Error obj = crearObj(errorMain, Desc, atributos);
-								lista.add(obj);
-								System.out.println(errorMain);
-							}
+					errorMain = cadena.substring(0, 45);
+					Desc = cadena.substring(45, cadena.length());
+				} else if (esError3(cadena, patternError3)) {
+					errorMain = cadena.substring(0, 43);
+					Desc = cadena.substring(43, cadena.length());
+				} else if (esInfo(cadena, patternInfo)) {
+					break;
+				} else if (esWar(cadena, patternWar)) {
+					break;
+				}
+				while ((cadena = br.readLine()) != null) {
+					if (esErrorP(cadena, patternErrorP) || esError(cadena, patternError) || esInfo(cadena, patternInfo)
+							|| esWar(cadena, patternWar) || esError2(cadena, patternError2)
+							|| esError3(cadena, patternError3))
+						break;
+					else {
+						String atributos = atributo(cadena);
+						if (atributos != null) {
+							bw.write(errorMain + "---" + atributos + "\n");
+							Error obj = crearObj(errorMain, Desc, atributos);
+							lista.add(obj);
+							System.out.println(errorMain);
 						}
 					}
 				}
+			}
 		}
-		// ImprimirObj(lista);
-		ferrores.close();
-		mimomo.close();
-		b.close();
-		lista.sort( new Comparator<Error>() {
+		bw.close();
+		isw.close();
+		fos.close();
+		bw2.close();
+		isw2.close();
+		fos2.close();
+		br.close();
+		isr.close();
+		fis.close();
+		lista.sort(new Comparator<Error>() {
 
 			@Override
 			public int compare(Error o1, Error o2) {
 				return o1.getError().compareTo(o2.getError());
 			}
-			
-			
+
 		});
 		return lista;
 	}
+	
+	
+	
+	
 
 	static void ImprimirObj(ArrayList<Error> lista) {
 		for (int i = 0; i < lista.size(); i++) {
@@ -232,7 +226,7 @@ public class Inicio {
 
 		} else
 			return false;
-		if (pa.matcher(a).find()) {			
+		if (pa.matcher(a).find()) {
 			return true;
 		}
 		return false;
@@ -258,7 +252,8 @@ public class Inicio {
 		} else
 			return false;
 		if (pa.matcher(a).find()) {
-			return true;}
+			return true;
+		}
 		return false;
 	}
 
@@ -271,7 +266,8 @@ public class Inicio {
 			return false;
 		if (pa.matcher(a).find()) {
 			System.out.println(a);
-			return true;}
+			return true;
+		}
 		return false;
 	}
 
